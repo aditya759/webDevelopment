e0605021718acf4f52376dee6598cfb77058cb91
aditya759 <40496002+aditya759@users.noreply.github.com>
2020-05-11 23:14:06 +0530

    Update script.js

--- assignmet4/script.js
+++ assignmet4/script.js
@@ -1,1 +1,16 @@
+ var names= ["Yakov","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
 
+ for(var i=0;i<names.length;i++)
+ {var name=names[i];
+
+     if(names[i].charAt(0)==="J")
+     {byeSpeaker.speak(name);
+
+     }
+     
+     else
+     {
+        helloSpeaker.speak(name);
+     }
+    
+ }
